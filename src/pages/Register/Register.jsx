56abd589cdcedd8108f913f7d648eea3ddcf5665
Milot Qorrolli <milot.qorrolli@student.uni-pr.edu>
2024-03-28 19:19:40 +0100

    added-validation-for-login-and-register
    User cannot login or register without filling all the textfields. Also, in the email textfield in the register page the user cannot enter other in email format

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,53 +1,44 @@
-import React from 'react'
-import Banner from '../../components/LoginBanner/Banner'
-import { NavLink } from 'react-router-dom'
-import './register.scss'
-
+import React from "react";
+import Banner from "../../components/LoginBanner/Banner";
+import { NavLink } from "react-router-dom";
+import "./register.scss";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className='container'>
-      <div className='wrapper1'>
+    <div className="container">
+      <div className="wrapper1">
         <Banner />
       </div>
-      <div className="wrapper2">
+      <form className="wrapper2" onSubmit={handleSubmit}> 
         <h1>Register</h1>
-        <div className='textfields'>
-          
+
+        <div className="textfields">
+          <div className="inputContainer">
+            <input type="text" placeholder="Full Name" className="inputBox" required/>
+            <label className="errorLabel"></label>
+          </div>
+          <div className="inputContainers">
+            <div className="inputContainer">
+              <input type="email" placeholder="Email" className="inputBox" required/>
+              <label className="errorLabel"></label>
+            </div>
+            <div className="inputContainer">
+              <input type="text" placeholder="Username" className="inputBox" required/>
+              <label className="errorLabel"></label>
+            </div>
             <div className="inputContainer">
               <input
-                type='text'
-                placeholder="Full Name"
+                placeholder="Password"
+                type="password"
                 className="inputBox"
+                required
               />
               <label className="errorLabel"></label>
             </div>
-          <div className='inputContainers'>
-          <div className="inputContainer">
-            <input
-              type="email"
-              placeholder="Email"
-              className="inputBox"
-            />
-            <label className="errorLabel"></label>
-          </div>
-          <div className="inputContainer">
-            <input
-              type='text'
-              placeholder="Username"
-              className="inputBox"
-            />
-            <label className="errorLabel"></label>
-          </div>
-          <div className="inputContainer">
-            <input
-              placeholder="Password"
-              type="password"
-              className="inputBox"
-            />
-            <label className="errorLabel"></label>
-          </div>
-          {/* <div className="inputContainer">
+            {/* <div className="inputContainer">
             <input
               placeholder="Confirm your password"
               type="password"
@@ -56,26 +47,18 @@ const Register = () => {
             <label className="errorLabel"></label>
           </div> */}
           </div>
-
         </div>
         <div className="registerContainer">
-            <p>
-              Already have an account? Login {" "}
-              <NavLink to='/login'>here</NavLink>
-            </p>
-          </div>
-          <div className="inputContainer">
-            <input
-              className="inputButton"
-              type="button"
-              value="Register"
-            />
-          </div>
+          <p>
+            Already have an account? Login <NavLink to="/login">here</NavLink>
+          </p>
         </div>
-
-        
+        <div className="inputContainer">
+          <input className="inputButton" type="submit" value="Register" />
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
